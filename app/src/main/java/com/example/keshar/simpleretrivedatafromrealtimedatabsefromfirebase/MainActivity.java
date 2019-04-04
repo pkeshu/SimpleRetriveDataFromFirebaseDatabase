952731cf8667b12fa8cb515465caaf077cd4066f
Keshar <paudelkeshar2055@gmail.com>
2019-04-04 17:45:44 +0545

    Simple Retrive data from Firebase database app is created.

---
+++ app/src/main/java/com/example/keshar/simpleretrivedatafromrealtimedatabsefromfirebase/MainActivity.java
@@ -0,0 +1,13 @@
+git remote add origin https://github.com/pkeshu/SimpleRetriveDataFromFirebaseDatabase.gitpackage com.example.keshar.simpleretrivedatafromrealtimedatabsefromfirebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
