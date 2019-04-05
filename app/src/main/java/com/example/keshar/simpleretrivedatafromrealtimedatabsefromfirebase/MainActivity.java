c96ea221e9faabcbce582baf9fb63ef41082f45a
Keshar <paudelkeshar2055@gmail.com>
2019-04-05 16:08:24 +0545

    Retrive data from realtime firebase database.

--- app/src/main/java/com/example/keshar/simpleretrivedatafromrealtimedatabsefromfirebase/MainActivity.java
+++ app/src/main/java/com/example/keshar/simpleretrivedatafromrealtimedatabsefromfirebase/MainActivity.java
@@ -1,13 +1,37 @@
-git remote add origin https://github.com/pkeshu/SimpleRetriveDataFromFirebaseDatabase.gitpackage com.example.keshar.simpleretrivedatafromrealtimedatabsefromfirebase;
+package com.example.keshar.simpleretrivedatafromrealtimedatabsefromfirebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView retriveData;
+    private Firebase firebaseUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+        retriveData=findViewById(R.id.retrive_data);
+        firebaseUrl=new Firebase("https://applicationdemo-eff34.firebaseio.com/Name/Keshar/");
+
+        firebaseUrl.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value=dataSnapshot.getValue(String.class);
+                retriveData.setText(value);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
     }
 }
